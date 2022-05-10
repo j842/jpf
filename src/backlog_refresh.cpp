16cf49636e702935ba616af53f2c858865945f44
j842 <j@jde.nz>
2022-05-10 21:05:21 +1200

    skeleton of refresh

--- src/backlog_refresh.cpp
+++ src/backlog_refresh.cpp
@@ -2,5 +2,17 @@
 
 void backlog::refresh()
 {
-    
+    { // remove any unused refrences.
+        std::vector<int> refcount(mItems.size(), 0);
+        for (auto &i : mItems)
+            for (auto &j : i.mDependencies)
+            {
+                unsigned int n = getItemIndexFromId(j);
+                refcount[n] += 1;
+            }
+
+        for (unsigned int ii = 0; ii < mItems.size(); ii++)
+            if (refcount[ii] == 0)
+                mItems[ii].mId = "";
+    }
 }
