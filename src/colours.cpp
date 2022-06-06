22bc751df540f4e7b4bbfb6af4f49203f6d6413f
j842 <j@jde.nz>
2022-06-06 18:03:28 +1200

    bump ver

--- src/colours.cpp
+++ src/colours.cpp
@@ -135,6 +135,18 @@ colourcode::colourcode(std::string console, std::string html) : console_code(con
     
 }
 
+const colourcode cWhite("\e[38;5;253m", "black");
+const colourcode cLightOrange("\e[38;5;179m", "orange");
+const colourcode cBlue("\e[38;5;69m", "blue");
+const colourcode cLime("\e[38;5;150m", "green");
+const colourcode cNoColour("\033[m", "</span>");
+
+const colourcode cInfo("\e[38;5;111m", "blue");
+const colourcode cDebug("\e[38;5;239m", "grey");
+const colourcode cWarning("\e[38;5;179m", "darkorange");
+const colourcode cError("\e[38;5;9m", "red");
+const colourcode cDefault("\e[38;5;118m", "black");
+
 
 } // namespace
 
--- src/colours.h
+++ src/colours.h
@@ -25,19 +25,17 @@ namespace colours
         std::string html_code;
     };
 
+    extern const colourcode cWhite;
+    extern const colourcode cLightOrange;
+    extern const colourcode cBlue;
+    extern const colourcode cLime;
+    extern const colourcode cNoColour;
 
-
-    const colourcode cWhite("\e[38;5;253m", "black");
-    const colourcode cLightOrange("\e[38;5;179m", "orange");
-    const colourcode cBlue("\e[38;5;69m", "blue");
-    const colourcode cLime("\e[38;5;150m", "green");
-    const colourcode cNoColour("\033[m", "</span>");
-
-    const colourcode cInfo("\e[38;5;111m", "blue");
-    const colourcode cDebug("\e[38;5;239m", "grey");
-    const colourcode cWarning("\e[38;5;179m", "orange");
-    const colourcode cError("\e[38;5;9m", "red");
-    const colourcode cDefault("\e[38;5;118m", "black");
+    extern const colourcode cInfo;
+    extern const colourcode cDebug;
+    extern const colourcode cWarning;
+    extern const colourcode cError;
+    extern const colourcode cDefault;
 }
 
 #endif
