7a4666b0e7c4210e5aaa599fb2d01dc910976ce0
j842 <j@842.be>
2024-10-17 10:21:01 +1300

    Build in docker.

--- gsheet/build.sh
+++ gsheet/build.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 SCRIPTDIR=$( dirname "$(readlink -f "$0")" )
 
+echo "------------------------------------------------------------------"
+echo "                    BUILDING build image"
+echo "------------------------------------------------------------------"
+
+docker build -f Dockerfile.build ${SCRIPTDIR} -t j842/jpfbuild
+
+echo "------------------------------------------------------------------"
+echo "                    COMPILING jpf"
+echo "------------------------------------------------------------------"
+
+docker run --rm -v ${SCRIPTDIR}/../:/code j842/jpfbuild
 cp "${SCRIPTDIR}/../src/build/jpf" "${SCRIPTDIR}/copy/"
 
-docker build ${SCRIPTDIR} -t j842/jpf
+echo "------------------------------------------------------------------"
+echo "                    BUILDING jpf image"
+echo "------------------------------------------------------------------"
+
+docker build -f Dockerfile.gsheet ${SCRIPTDIR} -t j842/jpf
 
--- gsheet/copy/docker-entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-/etc/init.d/fcgiwrap start
-chmod 766 /var/run/fcgiwrap.socket
-nginx -g "daemon off;"
